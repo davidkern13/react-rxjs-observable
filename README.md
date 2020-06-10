1902facb20093e0cf4590275e978f5b7adeaefac
David Kern <david33belle@gmail.com>
2020-06-10 09:56:25 +0300

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# react-rxjs-observable
-Created with CodeSandbox
---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React from "react";
+import axios from "axios";
+import { Observable } from "rxjs";
+
+export default function App() {
+  const observable = new Observable(subscriber => {
+    //demo server api request
+
+    axios
+      .get("https://fakerestapi.azurewebsites.net/api/Books")
+      .then(response => {
+        //for first the server return default value
+        subscriber.next({ data: "its take some time" });
+
+        //then after some time server return the all data
+        setTimeout(() => {
+          subscriber.next(response);
+          subscriber.complete();
+        }, 5600);
+      });
+  });
+
+  observable.subscribe({
+    next(x) {
+      console.log("got value " + JSON.stringify(x));
+    },
+    error(err) {
+      console.error("something wrong occurred: " + err);
+    },
+    complete() {
+      console.log("done");
+    }
+  });
+
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
